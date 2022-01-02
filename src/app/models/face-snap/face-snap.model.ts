aec6c5007e287614eeee742a4d89229dd85de269
Thomas Maulon <thomas.maulon@gmail.com>
2022-01-02 18:27:22 +0100

    condition the display of certain elements

--- src/app/models/face-snap/face-snap.model.ts
+++ src/app/models/face-snap/face-snap.model.ts
@@ -4,6 +4,7 @@ export class FaceSnap {
     public description: string,
     public imageUrl: string,
     public createdDate: Date,
-    public snaps: number
+    public snaps: number,
+    public location?: string
   ) {}
 }
