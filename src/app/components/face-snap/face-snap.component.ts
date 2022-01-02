8c3f821667238946bcf13bb6079743cd31381151
Thomas Maulon <thomas.maulon@gmail.com>
2022-01-02 17:44:14 +0100

    display some data with string interpolation and attribute binding

--- src/app/components/face-snap/face-snap.component.ts
+++ src/app/components/face-snap/face-snap.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  styleUrls: ['./face-snap.component.scss'],
 })
 export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.title = 'Ted';
+    this.description = 'Ted est une peluche plutôt funky!!';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl =
+      'https://media.senscritique.com/media/000006424972/1200/Ted.jpg';
   }
-
 }
