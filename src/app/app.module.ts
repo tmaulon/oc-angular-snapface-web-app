5786bfb1ef761308d390a757b6164631adf8d585
Thomas Maulon <thomas.maulon@gmail.com>
2022-01-02 16:29:39 +0100

    create a face snap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FaceSnapComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
