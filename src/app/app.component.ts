3f18e9c1e58abb03f230810d766fffbb8d380702
Thomas Maulon <thomas.maulon@gmail.com>
2022-01-02 18:18:57 +0100

    add custom properties and a face snap model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Snapface • Application de partage de photos';
+  myFaceSnap!: FaceSnap;
+
+  ngOnInit(): void {
+    this.myFaceSnap = new FaceSnap(
+      'Ted',
+      'Ted est une peluche plutôt funky!!',
+      'https://media.senscritique.com/media/000006424972/1200/Ted.jpg',
+      new Date(),
+      0
+    );
+  }
 }
--- src/app/components/face-snap/face-snap.component.ts
+++ src/app/components/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from './../../models/face-snap/face-snap.model';
 
 type FaceSnapButtonTextType = 'Oh Snap!' | 'Oops, unSnap!';
 
@@ -8,31 +9,21 @@ type FaceSnapButtonTextType = 'Oh Snap!' | 'Oops, unSnap!';
   styleUrls: ['./face-snap.component.scss'],
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
-  buttonText!: FaceSnapButtonTextType;
+  @Input() faceSnap!: FaceSnap;
+  public buttonText!: FaceSnapButtonTextType;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.title = 'Ted';
-    this.description = 'Ted est une peluche plutôt funky!!';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl =
-      'https://media.senscritique.com/media/000006424972/1200/Ted.jpg';
     this.buttonText = 'Oh Snap!';
   }
 
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.buttonText = 'Oops, unSnap!';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh Snap!';
     }
   }
