8428d7c6f2552ee37f46d4e1c8fbfc317bcd4808
Thomas Maulon <thomas.maulon@gmail.com>
2022-01-03 09:28:08 +0100

    adding some pipes to formate text and date, and add fr default locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './components/face-snap/face-snap.component';
@@ -6,7 +8,16 @@ import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 @NgModule({
   declarations: [AppComponent, FaceSnapComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
